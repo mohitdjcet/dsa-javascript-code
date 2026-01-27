33b0a07602583d07f234f0286c5a6b58299e4c2e
Mohit Kumar <mks.djcet@gmail.com>
2026-01-27 15:48:24 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-console.log("DSA Full Series – Step by Step");
-console.log("Main branch is used throughout this playlist");
-console.log("Check the Git branch name shown at the bottom-left of the video");
-console.log("Subscribe & Code with YouTube: Mohit Decodes");
+// Input: 45678
+// Output:5
+
+// let num = 45678;
+// let count = 0;
+
+// while( num>0){
+//     num = Math.floor(num/10);
+//     count++
+// }
+
+// console.log(count);
+
+
+
+//Reverse a Number
+//Input:1234
+//Output:4321
+
+let num = 1234;
+let reverse = 0;
+
+while( num>0){
+    let lastDigit = num % 10;
+    reverse = reverse*10 +lastDigit;
+    num = Math.floor(num/10);
+}
+
+console.log(reverse);
