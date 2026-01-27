368aa30961bcff2302c45a1dce3131f7287ed989
Mohit Kumar <mks.djcet@gmail.com>
2026-01-27 17:57:53 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-// Input: 45678
-// Output:5
-
-// let num = 45678;
-// let count = 0;
-
-// while( num>0){
-//     num = Math.floor(num/10);
-//     count++
-// }
-
-// console.log(count);
-
-
-
-//Reverse a Number
-//Input:1234
-//Output:4321
-
-let num = 1234;
+let num =121;
+let original = num;
 let reverse = 0;
 
 while( num>0){
-    let lastDigit = num % 10;
+    let lastDigit = num%10;
     reverse = reverse*10 +lastDigit;
     num = Math.floor(num/10);
 }
 
-console.log(reverse);
+if( original === reverse){
+    console.log("Palindrome Number");
+}else{
+    console.log("Not a Palindrome Number");
+    
+}
