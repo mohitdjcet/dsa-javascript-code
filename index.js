16bda682ea32800159786da18c6470eee286df8d
Mohit Kumar <mks.djcet@gmail.com>
2026-02-04 14:18:27 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-//Input= "hello"
-//Output= "olleh"
-
-let str = "hello";
-let strArr = str.split("");
-
+let arr = [1,2,3,4];
 let start = 0;
-let end = strArr.length - 1;
+let end = arr.length-1;
 
-while(start<end){
-    let temp = strArr[start];
-    strArr[start] = strArr[end];
-    strArr[end] = temp;
+while(start < end){
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
 
     start++;
     end--;
 }
 
-console.log(strArr.join(""));
-
+console.log(arr);
