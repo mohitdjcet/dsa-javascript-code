58cb3ec3f3473605f7c70850971cd4a5a1cec669
Mohit Kumar <mks.djcet@gmail.com>
2026-02-05 16:11:55 +0530

    updated code

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-let arr = [1,2,2,3,4,4,5];
+function reverse(arr, start,end){
+    //Two Pointer
+    while(start < end){
+        let temp = arr[start];
+        arr[start]= arr[end];
+        arr[end]= temp;
 
-// let uniqeArr = [...new Set(arr)];
-
-// console.log(uniqeArr);
-
-let uniqeArr = [];
-
-for(let i =0; i < arr.length; i++){
-    if(!uniqeArr.includes(arr[i])){
-        uniqeArr.push(arr[i]);
+        start++;
+        end--;
     }
 }
 
-console.log(uniqeArr);
+let arr = [1,2,3,4,5];
+let k=2;
+k = k%arr.length; //Edge Case Handle
+
+
+// reverse(arr,0,k-1);
+// reverse(arr,k,arr.length-1);
+// reverse(arr,0,arr.length-1);
+
+reverse(arr,0,arr.length-1);
+reverse(arr,0,k-1);
+reverse(arr,k,arr.length-1);
+
+console.log(arr);
