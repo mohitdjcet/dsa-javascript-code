16f63998f58f01a8bc8d4e59a77a8c5f1266f059
Mohit Kumar <mks.djcet@gmail.com>
2026-02-09 14:53:42 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function charFre(str){
-    let freq = {};
-    str = str.toLowerCase().replace(/\s+/g,"");
+// function maxSubArraySum(arr,k){
+//  let windowSum = 0;
+//  let maxSum = 0;
+// //first window
+//  for(let i =0; i<k; i++){
+//     windowSum += arr[i]
+//  }
 
-    for(let ch of str){
-        freq[ch]= (freq[ch] || 0)+1;
+//  maxSum = windowSum;
+// //Sliding window
+// for( let i =k; i<arr.length; i++){
+//     windowSum += arr[i]; //add next element
+//     windowSum -= arr[i-k]; //remove left element
+//     maxSum = Math.max(maxSum, windowSum);
+// }
+// return maxSum;
+// }
+// console.log(maxSubArraySum([2,1,5,1,3,2],3));
+
+
+function longUniqSubString(s){
+    let set = new Set();
+    let left = 0;
+    let maxLen =0;
+
+    for(let right = 0 ; right<s.length; right++){
+        while(set.has(s[right])){
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLen = Math.max(maxLen, right-left+1)
     }
-    return freq;
+    return maxLen;
 }
-
-console.log(charFre("Hello World"));
+console.log(longUniqSubString("abcabcbb"));
