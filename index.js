64b3f51b912956f9a2fdf3b218634ac1075f5d1d
Mohit Kumar <mks.djcet@gmail.com>
2026-02-09 16:23:12 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-// function maxSubArraySum(arr,k){
-//  let windowSum = 0;
-//  let maxSum = 0;
-// //first window
-//  for(let i =0; i<k; i++){
-//     windowSum += arr[i]
-//  }
+function buildPrefixSum(arr){
+    let prefix = new Array(arr.length);
+    prefix[0] = arr[0];
 
-//  maxSum = windowSum;
-// //Sliding window
-// for( let i =k; i<arr.length; i++){
-//     windowSum += arr[i]; //add next element
-//     windowSum -= arr[i-k]; //remove left element
-//     maxSum = Math.max(maxSum, windowSum);
-// }
-// return maxSum;
-// }
-// console.log(maxSubArraySum([2,1,5,1,3,2],3));
-
-
-function longUniqSubString(s){
-    let set = new Set();
-    let left = 0;
-    let maxLen =0;
-
-    for(let right = 0 ; right<s.length; right++){
-        while(set.has(s[right])){
-            set.delete(s[left]);
-            left++;
-        }
-        set.add(s[right]);
-        maxLen = Math.max(maxLen, right-left+1)
+    for(let i = 1; i<arr.length; i++){
+        prefix[i] = prefix[i-1]+arr[i];
     }
-    return maxLen;
+    return prefix;
 }
-console.log(longUniqSubString("abcabcbb"));
+
+console.log(buildPrefixSum([2,4,1,3,5]));
