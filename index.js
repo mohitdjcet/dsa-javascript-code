8ff674482a7fa32afc306c4c5c68b0affe4930b8
Mohit Kumar <mks.djcet@gmail.com>
2026-02-23 16:57:20 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-function isValid(s){
+function nextGreEle(arr){
+    let n = arr.length;
+    let res = new Array(n).fill(-1);
     let stack = [];
-    let map = {
-        ')':'(',
-        '}':'{',
-        ']':'['
-    };
 
-    for(let ch of s){
-        if(ch === '(' || ch === '{' | ch === '['){
-            stack.push(ch);
-        } else{
-            if(stack.length === 0) return false;
-
-            let top = stack.pop();
-            if(top !== map[ch]) return false
+    for(let i = n-1; i>=0; i--){
+        //POP smaller elements
+        while(stack.length >0 && stack[stack.length-1] <= arr[i]){
+            stack.pop();
         }
+        //Top of stack is next grater(if exist)
+        if(stack.length>0){
+            res[i] = stack[stack.length -1];
+        }
+
+        //Push current element
+        stack.push(arr[i]);
     }
-    return stack.length === 0;
+    return res;
 }
-
-console.log(isValid("(){}[]"));
-console.log(isValid("([)]"));
-
+console.log(nextGreEle([4,5,2,10,8]));
