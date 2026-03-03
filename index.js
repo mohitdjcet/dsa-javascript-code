a70071af248eae3230e54457c7443c5cc78debee
Mohit Kumar <mks.djcet@gmail.com>
2026-03-03 13:01:44 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,14 +1,89 @@
-let queue = [];
+function createCircularQueue(size){
+    let queue = new Array(size);
+    let front = -1;
+    let rear = -1;
 
-//Enqueue
-queue.push(10);
-queue.push(20);
-queue.push(30);
+    function isFull(){
+        return (rear+1)%size === front;
+    }
 
-//Front element
-console.log(queue[0]);
+    function isEmpty(){
+        return front === -1;
+    }
 
-//Dequeue
-queue.shift();
+    function enqueue(val){
+        if(isFull()){
+            console.log("Queue is Full");
+            return;
+        }
+        if(isEmpty()){
+            front = 0;
+            rear = 0;
+        } else {
+            rear = (rear+1 )% size;
+        }
 
-console.log(queue);
+        queue[rear] = val;
+    }
+
+    function dequeue(){
+        if(isEmpty()){
+            console.log("Queue is Empty");
+            return null;
+        }
+
+        let removed = queue[front];
+
+        if(front === rear){
+            front = -1;
+            rear = -1;
+        } else {
+            front = (front+1)% size;
+        }
+
+        return removed;
+    }
+
+    function peek(){
+        if(isEmpty()) return null;
+        return queue[front];
+    }
+
+    function print(){
+        if(isEmpty()){
+            console.log("Queue is Empty");
+            return null;
+        }
+
+        let result = [];
+        let i = front;
+
+        while(true){
+            result.push(queue[i])
+
+                if(i === rear) break;
+
+                i = (i+1)% size;
+        }
+        console.log(result);
+    }
+
+    return { enqueue, dequeue, peek, print}
+}
+
+
+let cq = createCircularQueue(5);
+
+cq.enqueue(10);
+cq.enqueue(20);
+cq.enqueue(30);
+cq.enqueue(40);
+
+cq.print();
+
+cq.dequeue();
+
+cq.enqueue(50);
+cq.enqueue(60);
+cq.enqueue(70);
+cq.print();
