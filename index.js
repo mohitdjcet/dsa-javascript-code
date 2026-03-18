f83e186250fa8e5ed4d6f3a86ce38a2e79a1f9fb
Mohit Kumar <mks.djcet@gmail.com>
2026-03-18 21:35:21 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,80 +1,36 @@
-class Node {
-  constructor(data) {
-    this.data = data; 
-    this.next = null;
+// // let map ={};
+
+// // map["name"]="Mohit";
+// // map["age"]= 28;
+
+// // console.log(map["age"])
+
+// let map = new Map();
+
+// map.set("name","Mohit");
+// map.set("age",28);
+
+
+// // console.log(map.get("age"))
+
+// // console.log(map.has("city"));
+// // console.log(map.delete("age"));
+// // console.log(map.get("age"))
+
+// console.log(map.size)
+
+function twoSum(arr,target){
+  let map = {};
+
+  for(let i =0; i<arr.length;i++){
+    let comp = target-arr[i];
+
+    if(map[comp] != undefined){
+      return [map[comp],i]
+    }
+    map[arr[i]]= i;
   }
+  return [];
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  // Insert at end
-  append(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-      return;
-    }
-
-    let current = this.head;
-    while (current.next) {
-      current = current.next;
-    }
-
-    current.next = newNode;
-  }
-
-  mergeList(l1,l2){
-    let dummy = new Node(-1);
-    let current = dummy;
-
-    while(l1 && l2){
-      if(l1.data < l2.data){
-        current.next = l1;
-        l1= l1.next;
-      } else {
-        current.next = l2;
-        l2 = l2.next;
-      }
-      current = current.next;
-    }
-
-
-    current.next = l1 || l2;
-
-    return dummy.next;
-  }
-
-  // Print list from any head
-  printFrom(head) {
-    let current = head;
-    let result = "";
-
-    while (current) {
-      result += current.data + " → ";
-      current = current.next;
-    }
-
-    console.log(result + "null");
-  }
-}
-
-let list1 = new LinkedList();
-list1.append(1);
-list1.append(3);
-list1.append(5);
-
-let list2 = new LinkedList();
-list2.append(2);
-list2.append(4);
-list2.append(6);
-
-list1.printFrom(list1.head);
-list2.printFrom(list2.head);
-
-let mergeHead=list1.mergeList(list1.head,list2.head);
-
-list1.printFrom(mergeHead);
+console.log(twoSum([2,7,11,15],13))
