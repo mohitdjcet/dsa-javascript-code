0fa5ddbfecbb37df5856ef0b6cedf0fa84610331
Mohit Kumar <mks.djcet@gmail.com>
2026-06-30 12:47:01 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// function bubbleSort(arr){
-//     let n = arr.length;
+function selectionSort(arr){
+    let n = arr.length;
 
-//     for(let i =0; i <n-1; i++){
-//         for(let j =0; j<n-1; j++){
-//             if(arr[j]>arr[j+1]){
-//                 let temp = arr[j];
-//                 arr[j] = arr[j+1];
-//                 arr[j+1]= temp
-//             }
-//         }
-//     }
-//     return arr;
-// }
-// console.log(bubbleSort([5,3,8,4,2]));
-
+    for(let i = 0; i< n-1; i++){
+        let minIndex = i;
+        for(let j =i+1;j<n; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex= j;
+            }
+        }
+        if(minIndex !== i){
+            [arr[i],arr[minIndex]]= [arr[minIndex],arr[i]];
+        }
+    }
+    return arr;
+}
+console.log(selectionSort([64,25,12,22,11]));
