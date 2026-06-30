2d1925edcd149518c41e5e63a22af915e7d8469a
Mohit Kumar <mks.djcet@gmail.com>
2026-06-30 13:02:04 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-function selectionSort(arr){
+function insertionSort(arr){
     let n = arr.length;
 
-    for(let i = 0; i< n-1; i++){
-        let minIndex = i;
-        for(let j =i+1;j<n; j++){
-            if(arr[j]<arr[minIndex]){
-                minIndex= j;
-            }
+    for(let i =1; i<n;i++){
+        let key = arr[i];
+        let j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]= arr[j];
+            j--;
         }
-        if(minIndex !== i){
-            [arr[i],arr[minIndex]]= [arr[minIndex],arr[i]];
-        }
+        arr[j+1]= key;
     }
     return arr;
 }
-console.log(selectionSort([64,25,12,22,11]));
+console.log(insertionSort([5,2,4,6,1,3]));
