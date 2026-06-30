2d5a2ab3f614411fc46af65f6e3364bac69bb7d5
Mohit Kumar <mks.djcet@gmail.com>
2026-06-30 14:07:08 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-function mergeSort(arr) {
+function quickSort(arr) {
   //Base Case
   if (arr.length <= 1) {
     return arr;
   }
-  let mid = Math.floor(arr.length / 2);
-  let left = arr.slice(0, mid);
-  let right = arr.slice(mid);
+  let pivot = arr[arr.length -1];
+  let left = [];
+  let right = [];
 
-  return merge(mergeSort(left), mergeSort(right));
+  for(let i =0; i<arr.length-1; i++){
+    if(arr[i]<pivot){
+        left.push(arr[i]);
+    }else{
+        right.push(arr[i]);
+    }
+  }
+  return [
+    ...quickSort(left),
+    pivot,
+    ...quickSort(right)
+  ]
 }
-
-function merge(left,right){
-    let result = [];
-    let i =0;
-    let j=0;
-    while (i < left.length && j< right.length){
-        if(left[i]<right[j]){
-            result.push(left[i]);
-            i++;
-        } else{
-            result.push(right[j]);
-            j++;
-        }
-    }
-    while(i < left.length){
-        result.push(left[i]);
-        i++;
-    }
-    while (j < right.length){
-        result.push(right[j]);
-        j++;
-    }
-    return result;
-}
-console.log(mergeSort([8,4,2,6]));
+console.log(quickSort([8, 3, 1, 7, 0, 10, 2]));
