8af52c736bf3ac2b1d17061729fd734dfa5ba857
Mohit Kumar <mks.djcet@gmail.com>
2026-07-05 15:02:15 +0530

    Updated code

--- index.js
+++ index.js
@@ -6,12 +6,45 @@ class TreeNode{
   }
 }
 
-let root = new TreeNode(10);
+let root = new TreeNode(1);
 
-root.left = new TreeNode(20);
-root.right = new TreeNode(30);
+root.left = new TreeNode(2);
+root.right =new TreeNode(3);
 
-root.left.left = new TreeNode(40);
-root.left.right = new TreeNode(50);
+root.left.left =new TreeNode(4);
+root.left.right =new TreeNode(5);
 
-console.log(root);
+root.right.left =new TreeNode(6);
+root.right.right = new TreeNode(7);
+
+// function preOrder(root){
+//   if(root === null){
+//     return;
+//   }
+//   console.log(root.value);
+//   preOrder(root.left);
+//   preOrder(root.right)
+  
+// }
+// preOrder(root);
+
+// function inOrder(root){
+//   if(root === null){
+//     return;
+//   }
+//   inOrder(root.left);
+//   console.log(root.value);
+//   inOrder(root.right)
+  
+// }
+// inOrder(root);
+
+function inOrder(root){
+  if(root === null){
+    return;
+  }
+  inOrder(root.left);
+  inOrder(root.right)
+  console.log(root.value);
+}
+inOrder(root);
