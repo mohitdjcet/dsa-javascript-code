153dccebce6049412358c258ffb956dc122144da
Mohit Kumar <mks.djcet@gmail.com>
2026-07-12 19:33:22 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-const graph = {
+const graph= {
   A: ["B","C"],
   B: ["A","D","E"],
   C: ["A","F"],
   D: ["B"],
   E: ["B"],
   F: ["C"]
-}
+};
 
-function bfs(graph, start){
-  let queue = [];
+// function dfs(graph,node,visited = new Set()){
+//   console.log(node);
+//   visited.add(node);
+//   for(let n of graph[node]){
+//     if(!visited.has(n)){
+//       dfs(graph,n,visited);
+//     }
+//   }
+// }
+// dfs(graph,"A")
+
+function dfs(graph,start){
+  let stack = [start];
   let visited = new Set();
 
-  queue.push(start);
-  visited.add(start);
-
-  while(queue.length >0){
-    let current = queue.shift();
-    console.log(current);
-    for(let neighbor of graph[current]){
-      if(!visited.has(neighbor)){
-        visited.add(neighbor);
-        queue.push(neighbor)
+  while(stack.length >0){
+    let current = stack.pop();
+    if(!visited.has(current)){
+      console.log(current);
+      visited.add(current);
+      for(let i = graph[current].length-1; i>=0; i--){
+        let neighbor = graph[current][i];
+        if(!visited.has(neighbor)){
+          stack.push(neighbor)
+        }
       }
     }
-    
   }
 }
-
-bfs(graph,"A");
+dfs(graph,"A")
