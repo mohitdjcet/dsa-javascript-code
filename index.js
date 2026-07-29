3f285cb0ec00885e4ed0eed6a459ce4b76e88b81
Mohit Kumar <mks.djcet@gmail.com>
2026-07-29 15:54:34 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-function sunsets(nums){
-    let result = [];
+const n = 4;
+const board = Array.from({length:n}, ()=> Array(n).fill("."));
 
-    function backtrack(index,current){
-        result.push([...current])
-        for(let i = index; i<nums.length; i++){
-            current.push(nums[i]);
-            backtrack(i+1,current);
-            current.pop();
+function isSafe(row,col){
+    for(let i =0; i<row; i++){
+        if(board[i][col] === "Q") return false
+    }
+    for(let i =row-1, j= col-1; i>=0&&j>=0;i--,j--){
+        if(board[i][j] === "Q") return false
+    }
+    for(let i =row-1, j= col+1; i>=0&&j<n;i--,j++){
+        if(board[i][j] === "Q") return false
+    }
+    return true
+}
+
+function solve(row){
+    if(row === n){
+        console.log("Solution Found");
+        console.log(board);
+        return;
+    }
+    for(let col=0;col<n;col++){
+        if(isSafe(row,col)){
+            board[row][col]= "Q";
+            solve(row+1);
+            board[row][col]= "."
         }
     }
-    backtrack(0,[]);
-    return result;
 }
-console.log(sunsets([1,2]));
+solve(0);
