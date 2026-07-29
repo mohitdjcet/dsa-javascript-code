ca7d7bcca1dcb12e56d64c93c154c8ad7af01b19
Mohit Kumar <mks.djcet@gmail.com>
2026-07-29 15:06:11 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-const graph= {
-  A: ["B","C"],
-  B: ["A","D","E"],
-  C: ["A","F"],
-  D: ["B"],
-  E: ["B"],
-  F: ["C"]
-};
+function coinChnage(amount){
+    const coins = [10,5,2,1];
+    let result = [];
 
-// function dfs(graph,node,visited = new Set()){
-//   console.log(node);
-//   visited.add(node);
-//   for(let n of graph[node]){
-//     if(!visited.has(n)){
-//       dfs(graph,n,visited);
-//     }
-//   }
-// }
-// dfs(graph,"A")
-
-function dfs(graph,start){
-  let stack = [start];
-  let visited = new Set();
-
-  while(stack.length >0){
-    let current = stack.pop();
-    if(!visited.has(current)){
-      console.log(current);
-      visited.add(current);
-      for(let i = graph[current].length-1; i>=0; i--){
-        let neighbor = graph[current][i];
-        if(!visited.has(neighbor)){
-          stack.push(neighbor)
+    for(let coin of coins){
+        while(amount >= coin){
+            result.push(coin);
+            amount -= coin;
         }
-      }
     }
-  }
+    return result;
 }
-dfs(graph,"A")
+console.log(coinChnage(28));
